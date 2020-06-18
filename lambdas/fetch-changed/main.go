cf4a4fdc6a6950784eea3e94de96cf06a7048808
Eric Evans <eevans@wikimedia.org>
2020-06-18 17:11:48 -0500

    fetch-changed: refactor to use common.Logger
    Replaces the various uses of `logDebug()` and `fmt.Printf` with the
    logger in `github.com/wikimedia/phoenix/common`; Log level is now
    configured using the `LOG_LEVEL` env var to one of `ERROR`, `WARN`,
    `INFO`, or `DEBUG`.
    
    Also:
      - Sets `Content-Type: text/html` on the HTML upload.
      - Various minor cleanups

--- lambdas/fetch-changed/main.go
+++ lambdas/fetch-changed/main.go
@@ -8,14 +8,12 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"strings"
 
 	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/wikimedia/phoenix/common"
@@ -24,7 +22,10 @@ import (
 
 const folderName string = "incoming"
 
-var debug bool = false
+var (
+	debug bool = false
+	log   *common.Logger
+)
 
 func keyf(msg *common.ChangeEvent) string {
 	return fmt.Sprintf("%s/%s/%s-%d", folderName, msg.ServerName, msg.Title, msg.Revision)
@@ -48,12 +49,6 @@ func getPage(msg *common.ChangeEvent) ([]byte, error) {
 	return body, nil
 }
 
-func logDebug(format string, v ...interface{}) {
-	if debug {
-		fmt.Printf("[DEBUG] %s\n", fmt.Sprintf(strings.TrimSuffix(format, "\n"), v...))
-	}
-}
-
 func handleRequest(ctx context.Context, event events.SNSEvent) {
 	s3client := s3.New(session.New(&aws.Config{
 		Region: aws.String(env.S3RawContentStorage().AWSConfig().Region()),
@@ -66,67 +61,59 @@ func handleRequest(ctx context.Context, event events.SNSEvent) {
 
 		msg := &common.ChangeEvent{}
 		if err := json.Unmarshal([]byte(snsRecord.Message), msg); err != nil {
-			fmt.Println("[Error] Unable to deserialize message payload:", err)
+			log.Error("Unable to deserialize message payload:", err)
 			continue
 		}
 
-		logDebug("%+v", msg)
+		log.Debug("Processing change event: %+v", msg)
 
 		page, err := getPage(msg)
 		if err != nil {
-			fmt.Printf("[Error] Unable to retrieve %s (%s)\n", urlf(msg), err)
+			log.Error("Unable to retrieve %s (%s)\n", urlf(msg), err)
 			continue
 		}
 
-		logDebug("Retrieved %s, %d bytes", urlf(msg), len(page))
+		log.Debug("Retrieved %s, %d bytes", urlf(msg), len(page))
 
-		input := &s3.PutObjectInput{
-			Body:   aws.ReadSeekCloser(bytes.NewReader(page)),
-			Bucket: aws.String(env.S3RawContentStorage().Name()),
-			Key:    aws.String(keyf(msg)),
-			Metadata: map[string]*string{
-				"title":       aws.String(msg.Title),
-				"server_name": aws.String(msg.ServerName),
-				"revision":    aws.String(fmt.Sprintf("%d", msg.Revision)),
-			},
-		}
-
-		logDebug("%+v", input)
-
-		result, err := s3client.PutObject(input)
+		result, err := s3client.PutObject(
+			&s3.PutObjectInput{
+				Body:        aws.ReadSeekCloser(bytes.NewReader(page)),
+				Bucket:      aws.String(env.S3RawContentStorage().Name()),
+				Key:         aws.String(keyf(msg)),
+				ContentType: aws.String("text/html"),
+				Metadata: map[string]*string{
+					"title":       aws.String(msg.Title),
+					"server_name": aws.String(msg.ServerName),
+					"revision":    aws.String(fmt.Sprintf("%d", msg.Revision)),
+				},
+			})
 		if err != nil {
-			if aerr, ok := err.(awserr.Error); ok {
-				switch aerr.Code() {
-				default:
-					fmt.Printf("[Error] %s: %s (%+v)\n", aerr.Code(), aerr.Message(), aerr.OrigErr())
-				}
-			} else {
-				// Print the error, cast err to awserr.Error to get the Code and
-				// Message from an error.
-				fmt.Println(err.Error())
-			}
+			log.Error("Unable to upload HTML document to S3: %s", err)
 			continue
 		}
 
-		logDebug("%+v", result)
+		log.Debug("HTML upload complete: %+v", result)
 
 		output, err := snsPub.SendChangeEvent(msg)
 		if err != nil {
-			fmt.Printf("[Error] Unable to send SNS change event: %s", err)
+			log.Error("Unable to send SNS change event: %s", err)
 			continue
 		}
 
-		logDebug("%+v", output)
+		log.Debug("SNS change event sent: %+v", output)
 	}
 }
 
 func init() {
-	if val, ok := os.LookupEnv("WMDEBUG"); ok {
-		if val != "0" || strings.ToLower(val) != "false" {
-			debug = true
-			fmt.Println("DEBUG LOGGING ENABLED (unset WMDEBUG env var to disable)")
-		}
+	// Determine logging level
+	var level string = "ERROR"
+	if v, ok := os.LookupEnv("LOG_LEVEL"); ok {
+		level = v
 	}
+
+	// Initialize the logger
+	log = common.NewLogger(level)
+	log.Warn("%s LOGGING ENABLED (use LOG_LEVEL env var to configure)", common.LevelString(log.Level))
 }
 
 func main() {
