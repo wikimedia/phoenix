7f2b146d47316fb0979fcadfa9b4c4ff4562a4f1
Peter Ovchyn <peter.ovchyn@speedandfunction.com>
2020-06-27 20:31:43 +0300

    Add vscode config to .gitignore Implement Paragraph parser

--- lambdas/transform-parsoid/main.go
+++ lambdas/transform-parsoid/main.go
@@ -1,13 +1,58 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
+	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"golang.org/x/net/html"
 )
+
+type SectionType uint32
+
+const (
+	HeaderSection SectionType = iota
+	ParagrpaphSection
+	ReferencesSection
+)
+
+type Paragraph struct {
+	Name    string     `json:"name"`
+	Content []*Content `json:"content"`
+}
+
+func newParagraph(name string, content []*Content) *Paragraph {
+	return &Paragraph{name, content}
+}
+
+type Header struct {
+	Content []*Content `json:"content"`
+}
+
+type Reference struct {
+}
+
+type Page struct {
+	Header     []*Content   `json:"header,omitempty"`
+	Paragraphs []*Paragraph `json:"paragraphs,omitempty"`
+	References []*Reference `json:"references,omitempty"`
+}
+
+func newPage() *Page {
+	new := Page{nil, []*Paragraph{}, []*Reference{}}
+	return &new
+}
+
+func (page *Page) AddParagraph(paragraph *Paragraph) {
+	page.Paragraphs = append(page.Paragraphs, paragraph)
+}
+
+func (page *Page) SetHeader(header []*Content) {
+	page.Header = header
+}
 
 // Content represents a piece of content on a page
 type Content struct {
@@ -80,20 +125,69 @@ func isIgnored(token html.Token) bool {
 	return ignoredTags[token.Data]
 }
 
-func requestParsoid(domain string, title string) (parsoidHTML string, err error) {
-	res, err := http.Get(urlf(domain, title))
+func sectionType(section *goquery.Selection) {
+
+}
+
+func parseParsoid(r io.Reader) (page *Page, err error) {
+	doc, err := goquery.NewDocumentFromReader(r)
 	if err != nil {
-		return
+		return nil, err
+	}
+	//page := newPage()
+	sections := doc.Find("html body section[data-mw-section-id]")
+	for i := range sections.Nodes {
+		fmt.Printf("Review %d:\n", i)
+		section := sections.Eq(i)
+		html, err := section.Html()
+		if err != nil {
+			continue
+		}
+
+		content, err := tokenizeHtml(strings.NewReader(html))
+		if err != nil {
+			continue
+		}
+		page := newPage()
+		name := section.Find("h2").First()
+		if len(name.Nodes) == 0 {
+			page.SetHeader(content.Children)
+			fmt.Println("header")
+		} else {
+			id, exists := name.Attr("id")
+			if !exists {
+				continue
+			}
+			if id == "Reference" {
+
+			} else {
+				page.AddParagraph(newParagraph(name.Text(), content.Children))
+			}
+		}
+
+	}
+	return page, nil
+}
+
+func processParsoid(domain string, title string) (page *Page, err error) {
+	url := urlf(domain, title)
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
 	}
 	defer res.Body.Close()
-	z := html.NewTokenizer(res.Body)
+	return parseParsoid(res.Body)
+}
+
+func tokenizeHtml(r io.Reader) (content *Content, err error) {
+	z := html.NewTokenizer(r)
 	parent := newContent("page", nil)
 	for {
 		switch z.Next() {
 		case html.ErrorToken:
-			bytes, _ := json.Marshal(parent)
-			fmt.Println(string(bytes))
-			return "", z.Err()
+			//bytes, _ := json.Marshal(parent)
+			//fmt.Println(z.Err())
+			return parent, nil
 		case html.TextToken:
 			text := newContent("text", parent)
 			text.Text = string(z.Text())
@@ -125,6 +219,22 @@ func requestParsoid(domain string, title string) (parsoidHTML string, err error)
 	}
 }
 
+func requestParsoid(domain string, title string) (content *Content, err error) {
+	res, err := http.Get(urlf(domain, title))
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	return tokenizeHtml(res.Body)
+
+}
+
 func main() {
-	requestParsoid("en.wikipedia.org", "United_States")
+	res, err := processParsoid("en.wikipedia.org", "Mars")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(res)
+	//requestParsoid("en.wikipedia.org", "United_States")
 }
