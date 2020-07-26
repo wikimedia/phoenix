2c2479b097cd73da1434bfe6c54f9e90f2050a77
Peter Ovchyn <peter.ovchyn@speedandfunction.com>
2020-07-26 21:49:07 +0300

    Implement parsing page and section

--- lambdas/transform-parsoid/main.go
+++ lambdas/transform-parsoid/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/wikimedia/phoenix/common"
 )
 
@@ -30,11 +35,6 @@ import (
 // 		attributes[attr.Key] = attr.Val
 // 	}
 // 	return attributes
-// }
-
-// // Use the existing endpoint for now for testing
-// func urlf(domain string, title string) string {
-// 	return fmt.Sprintf("https://%s/api/rest_v1/page/html/%s", domain, url.PathEscape(title))
 // }
 
 // var ignoredTags = map[string]bool{
@@ -120,8 +120,37 @@ import (
 // 	return tokenizeHtml(res.Body)
 
 // }
+// func preParseGetSections(document *goquery.Document) {
+
+// }
+
+// Use the existing endpoint for now for testing
+func urlf(domain string, title string) string {
+	return fmt.Sprintf("https://%s/api/rest_v1/page/html/%s", domain, url.PathEscape(title))
+}
+
+func requestParsoid(domain string, title string) (body io.ReadCloser, err error) {
+	res, err := http.Get(urlf(domain, title))
+	if err != nil {
+		return nil, err
+	}
+	return res.Body, nil
+}
 
 func main() {
+
+	bodyReader, err := requestParsoid("simple.wikipedia.org", "Mars")
+	if err != nil {
+		return
+	}
+	defer bodyReader.Close()
+
+	document, err := goquery.NewDocumentFromReader(bodyReader)
+	if err != nil {
+		return
+	}
+	parseParsoidDocument(document)
+
 	source := common.Source{
 		ID:        100,
 		Revision:  200,
@@ -144,6 +173,8 @@ func main() {
 		HasPart:      parts,
 		About:        about,
 	}
+
+	//common.Section
 
 	println(page.ID)
 }
