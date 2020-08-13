c4816b8c1d4e7f3491813bd84378673083711494
Moriel Schottlender <moriel@gmail.com>
2020-08-13 12:15:15 -0700

    Add request headers and console logging; fix indents
    Turns out adding `http://` is important even if the address is `localhost`.
    Also, send headers and adjust the way that the data is sent to the Go
    service.

--- demo/src/components/FormFetchByPageName.vue
+++ demo/src/components/FormFetchByPageName.vue
@@ -1,27 +1,24 @@
 <template>
   <div id="form-fetch-page">
-  <v-form v-model="valid">
-  <v-container>
-    <v-row>
-     <v-text-field
-      v-model="pageName"
-      :rules="pageRules"
-      label="Page name"
-      required
-      ></v-text-field>
-      <v-btn color="secondary" :disbled="!valid"
-        :loading="loading"
-        @click="fetch"
-      >Fetch</v-btn>
-    </v-row>
-  </v-container>
-  </v-form>
-  <v-container v-show="result">
-    {{result}}
-    </v-container>
+    <v-form v-model="valid">
+      <v-container>
+        <v-row>
+        <v-text-field
+          v-model="pageName"
+          :rules="pageRules"
+          label="Page name"
+          required
+          ></v-text-field>
+          <v-btn color="secondary" :disbled="!valid"
+            :loading="loading"
+            @click="fetch"
+          >Fetch</v-btn>
+        </v-row>
+      </v-container>
+    </v-form>
+    <v-container v-show="result">{{result}}</v-container>
     <v-alert v-show="error">{{error}}</v-alert>
   </div>
-
  </template>
 
 <script>
@@ -45,17 +42,23 @@ export default {
       this.loading = true
       this.result = null
       this.error = null
-      axios.post('localhost:8080/query', {
+      axios.post('http://localhost:8080/query', {
         query: 'query Page($name: String!) { pageByName(name: $name) { name dateModified hasPart about { key val } } }',
+        // TODO: The name should come from the search input
         variables: { name: 'Foobar' }
+      },
+      {
+        headers: { 'Content-Type': 'application/json' }
       })
         .then(res => {
           // TODO: Actually do something with this result
           this.result = res
           this.loading = false
+          console.log(res)
         })
         .catch(e => {
           this.error = e
+          console.log(e)
           this.loading = false
         })
     }
