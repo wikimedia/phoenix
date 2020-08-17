19dda1b6a96e948998b6454a9517953136892952
Moriel Schottlender <moriel@gmail.com>
2020-08-17 11:50:03 -0700

    Create a basic web UI for the content store
    - Created a vuejs-based web UI with tooling that can communicate
      with the GraphQL service.
    - Added capability to the GraphQL service to fetch by name, and
      overcome CORS requests.
    - This is first iteration basic representation. Read the README
      on how to kickstart the UI.
    
    Fixes #21

--- demo/src/components/FormFetchByPageName.vue
+++ demo/src/components/FormFetchByPageName.vue
@@ -19,18 +19,38 @@
         </v-row>
       </v-container>
     </v-form>
-    <v-container v-show="result">{{ result }}</v-container>
     <v-alert v-show="error">{{ error }}</v-alert>
+    <v-container v-if="result">
+      <v-tabs>
+        <v-tab href="#tab-page">Page</v-tab>
+        <v-tab href="#tab-payload">Payload</v-tab>
+        <v-tab-item value="tab-page">
+          <PageInfo v-if="result" :pagedata="result" />
+        </v-tab-item>
+        <v-tab-item value="tab-payload">
+          <v-container v-if="payload">
+            <v-card>
+              <v-card-text>
+                <pre>{{ payload }}</pre>
+              </v-card-text>
+            </v-card>
+          </v-container>
+        </v-tab-item>
+      </v-tabs>
+    </v-container>
   </div>
 </template>
 
 <script>
+import PageInfo from './PageInfo'
 import axios from 'axios'
 
 export default {
   name: 'FormFetchByPageName',
+  components: { PageInfo },
   data: () => ({
     error: null,
+    payload: null,
     result: null,
     valid: false,
     loading: false,
@@ -43,6 +63,7 @@ export default {
   methods: {
     fetch() {
       this.loading = true
+      this.payload = null
       this.result = null
       this.error = null
       axios
@@ -59,9 +80,15 @@ export default {
           }
         )
         .then(res => {
-          // TODO: Actually do something with this result
-          this.result = res
+          const data = res.data.data.pageByName
+          this.payload = JSON.stringify(res, null, 2)
+          this.result = {
+            title: data.name,
+            modified: data.dateModified,
+            parts: data.hasPart
+          }
           console.log(res)
+          console.log(this.result)
         })
         .catch(e => {
           this.error = e
---
+++ demo/src/components/PageInfo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="page-info">
+    <h1>
+      {{ pagedata.title }}
+    </h1>
+    <v-list two-line subheader>
+      <v-subheader inset>Metadata</v-subheader>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-icon>fa-calendar</v-icon>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>Last modified</v-list-item-title>
+          <v-list-item-subtitle v-text="humanReadable"></v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-subheader inset>Parts</v-subheader>
+      <v-list-item v-for="part in pagedata.parts" :key="part">
+        <v-list-item-avatar>
+          <v-icon>fa-puzzle-piece</v-icon>
+        </v-list-item-avatar>
+        <v-list-item-content>{{ part }}</v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  name: 'PageInfo',
+  props: { pagedata: Object },
+  computed: {
+    humanReadable() {
+      return moment(this.pagedata.modified).format('MMMM Do YYYY, h:mm:ss a')
+    }
+  }
+  // data: () => ({})
+}
+</script>
