376ee76d287d9631ca84bf8da633117a95cdb9b0
Eric Evans <eevans@wikimedia.org>
2020-08-26 19:19:59 -0500

    File logging
    Extends `common.Logger` to include a `NewFileLogger()` function for
    constructing file-based loggers.  Additionally, supports prefixing log
    entries with an ISO8660 formatted timestamp.
    
    Log created by the new constructor (`NewFileLogger`) default to using
    timestamps.  Stdout loggers (as created by `NewLogger`) do not have
    timestamps (this preserves backward compatability for Lambdas).

--- common/logging.go
+++ common/logging.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"strings"
+	"time"
 )
 
 // LogLevel is base type of log levels
@@ -19,12 +20,21 @@ const (
 	INFO LogLevel = 1
 	// DEBUG is a log level
 	DEBUG LogLevel = 0
+
+	// Ltimestamp is a flag used to indicate that timestamps should be included in log output
+	Ltimestamp = 1 << iota
 )
 
 // Logger formats messages to standard out, filtered according to the configured log level.
 type Logger struct {
 	Level  LogLevel
 	Output io.Writer
+	flags  int
+}
+
+// SetFlags sets the output flags for the logger. Currently, the only flag bit is Ltimestamp.
+func (l *Logger) SetFlags(flags int) {
+	l.flags = flags
 }
 
 // Error logs messages at the ERROR level
@@ -52,29 +62,54 @@ func (l *Logger) log(level LogLevel, format string, v ...interface{}) {
 		return
 	}
 
+	if (l.flags & Ltimestamp) == Ltimestamp {
+		fmt.Fprintf(l.Output, "%s ", time.Now().Format(time.RFC3339))
+	}
+
 	message := fmt.Sprintf(strings.TrimSuffix(format, "\n"), v...)
 	fmt.Fprintf(l.Output, "[%s] %s\n", LevelString(level), message)
 }
 
-// NewLogger creates new Logger instances.  Valid levels are ERROR, WARN, INFO, and DEBUG.
-func NewLogger(level string) *Logger {
-	var logger *Logger = &Logger{Output: os.Stdout}
-
+func (l *Logger) setLevel(level string) {
 	switch strings.ToUpper(level) {
 	case "ERROR":
-		logger.Level = ERROR
+		l.Level = ERROR
 	case "WARN":
-		logger.Level = WARN
+		l.Level = WARN
 	case "INFO":
-		logger.Level = INFO
+		l.Level = INFO
 	case "DEBUG":
-		logger.Level = DEBUG
+		l.Level = DEBUG
 	default:
-		logger.Level = ERROR
-		logger.Error("Logger initialized with unknown log level: %s (defaulting to ERROR)", level)
+		l.Level = ERROR
+		l.Error("Logger assigned unknown log level: %s (defaulting to ERROR)", level)
 	}
+}
+
+// NewLogger creates new Logger instances.  Valid levels are ERROR, WARN, INFO, and DEBUG.
+func NewLogger(level string) *Logger {
+	var logger *Logger = &Logger{Output: os.Stdout, flags: 0}
+
+	logger.setLevel(level)
 
 	return logger
+}
+
+// NewFileLogger creates new Logger instances.  Valid levels are ERROR, WARN, INFO, and DEBUG.
+func NewFileLogger(level, filename string) (*Logger, error) {
+	var logger *Logger
+	var file *os.File
+	var err error
+
+	if file, err = os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666); err != nil {
+		return nil, fmt.Errorf("unable to open %s for writing: %w", filename, err)
+	}
+
+	logger = &Logger{Output: file, flags: Ltimestamp}
+
+	logger.setLevel(level)
+
+	return logger, nil
 }
 
 // LevelString maps a LogLevel type to its string value
