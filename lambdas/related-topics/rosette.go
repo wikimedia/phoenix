d75f3fd4ef67a82748d455ab3dd5f1257b716848
Eric Evans <eevans@wikimedia.org>
2021-01-20 19:44:22 -0600

    Retry Rosette 429s (#90)
    Introduces bounded retries with an exponential backoff.

--- lambdas/related-topics/rosette.go
+++ lambdas/related-topics/rosette.go
@@ -8,8 +8,10 @@ import (
 	"net/http"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/jpillora/backoff"
 	"github.com/wikimedia/phoenix/common"
 )
 
@@ -17,6 +19,7 @@ const (
 	rosetteEndpoint    = "https://api.rosette.com/rest/v1/topics?redirect=true"
 	rosetteMinSalience = 0.1
 	rosetteMaxTopics   = 10
+	rosetteRetries     = 10
 )
 
 var (
@@ -45,45 +48,73 @@ type topicsResponse struct {
 }
 
 func requestTopics(text string) (*topicsResponse, error) {
+	var b = &backoff.Backoff{Min: 500 * time.Millisecond, Max: 20 * time.Second, Jitter: true}
 	var client = &http.Client{}
 	var err error
-	var req *http.Request
-	var reqData []byte
 	var res *http.Response
 	var resData []byte
 	var topics topicsResponse
 
-	// Serialize a requests body (JSON)
-	content := &struct {
-		Content string `json:"content"`
-	}{
-		text,
+	request := func(text string) (*http.Response, error) {
+		var e error
+		var req *http.Request
+		var reqData []byte
+		var r *http.Response
+
+		// Serialize a requests body (JSON)
+		content := &struct {
+			Content string `json:"content"`
+		}{
+			text,
+		}
+
+		if reqData, e = json.Marshal(content); e != nil {
+			return nil, fmt.Errorf("unable to serialize JSON request body: %w", e)
+		}
+
+		// Create the request
+		if req, e = http.NewRequest("POST", rosetteEndpoint, bytes.NewBuffer(reqData)); e != nil {
+			return nil, fmt.Errorf("failed to create HTTP request: %w", e)
+		}
+
+		req.Header.Set("X-RosetteAPI-Key", rosetteAPIKey)
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Accept", "application/json")
+		req.Header.Set("Cache-Control", "no-cache")
+
+		// Send the request
+		if r, e = client.Do(req); e != nil {
+			return nil, fmt.Errorf("HTTP request failed: %w", e)
+		}
+
+		return r, nil
 	}
 
-	if reqData, err = json.Marshal(content); err != nil {
-		return nil, fmt.Errorf("unable to serialize JSON request body: %w", err)
+Loop:
+	for i := 1; ; i++ {
+		// Send the request
+		if res, err = request(text); err != nil {
+			return nil, err
+		}
+
+		defer res.Body.Close()
+
+		switch res.StatusCode {
+		case http.StatusOK:
+			break Loop
+		case http.StatusTooManyRequests:
+			if i >= rosetteRetries {
+				return nil, fmt.Errorf("Reached max number of Rosette retries (%d)", rosetteRetries)
+			}
+
+			delay := b.Duration()
+			log.Debug("Rosette returned status 429; reconnecting in %s (#%d/%d)", delay, i, rosetteRetries)
+			time.Sleep(delay)
+			continue
+		default:
+			return nil, fmt.Errorf("unexpected HTTP status code %d (expected %d)", res.StatusCode, http.StatusOK)
+		}
 	}
-
-	// Create the request
-	if req, err = http.NewRequest("POST", rosetteEndpoint, bytes.NewBuffer(reqData)); err != nil {
-		return nil, fmt.Errorf("failed to create HTTP request: %w", err)
-	}
-
-	req.Header.Set("X-RosetteAPI-Key", rosetteAPIKey)
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("Cache-Control", "no-cache")
-
-	// Send the request
-	if res, err = client.Do(req); err != nil {
-		return nil, fmt.Errorf("HTTP request failed: %w", err)
-	}
-
-	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("unexpected HTTP status code %d (expected %d)", res.StatusCode, http.StatusOK)
-	}
-
-	defer res.Body.Close()
 
 	// Read the response
 	if resData, err = ioutil.ReadAll(res.Body); err != nil {
